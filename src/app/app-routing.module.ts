d7ccd3a7d8b42f2a768dc65b6d964dd155059592
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-25 12:18:59 -0300

    Gerando componente HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/components/home/home.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,13 +19,15 @@ import { MatTableModule } from '@angular/material/table';
 import { HeaderComponent } from './views/components/template/header/header.component';
 import { FooterComponent } from './views/components/template/footer/footer.component';
 import { NavComponent } from './views/components/template/nav/nav.component';
+import { HomeComponent } from './views/components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
