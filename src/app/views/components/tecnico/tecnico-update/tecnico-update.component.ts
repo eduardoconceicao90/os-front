964fa71efcc23eba8daa76ffcdbc5b8a21529a48
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-26 15:36:51 -0300

    componente update para Tecnico Parte 2

--- src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 
@@ -10,6 +10,8 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
   styleUrls: ['./tecnico-update.component.css']
 })
 export class TecnicoUpdateComponent implements OnInit {
+
+  id_tec = ''
 
   tecnico : Tecnico = {
     id: '',
@@ -22,12 +24,27 @@ export class TecnicoUpdateComponent implements OnInit {
   cpf = new FormControl('', [Validators.minLength(11)])
   telefone = new FormControl('', [Validators.minLength(10)])
 
-  constructor(private router : Router, private service : TecnicoService) { }
+  constructor(private router : Router, private service : TecnicoService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id_tec = this.route.snapshot.paramMap.get('id')!
+    this.findById();
   }
 
-  
+  findById(): void {
+    this.service.findById(this.id_tec).subscribe((resposta) => {
+      this.tecnico = resposta;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.tecnico).subscribe((resposta) =>{
+      this.router.navigate(['tecnicos'])
+      this.service.message('Técnico atualizado com sucesso!')
+    }, err => {
+      this.service.message('Validar se todos os campos estão preenchidos corretamente')
+    })
+  }
 
   errorValidNome() {
     if(this.nome.invalid){
