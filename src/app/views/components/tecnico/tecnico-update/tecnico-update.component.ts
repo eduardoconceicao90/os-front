7c8deae73a07a25ffa81b9c8aabcbfe59d00715d
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-26 14:34:53 -0300

    componente update para Tecnico Parte 1

---
+++ src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+
+@Component({
+  selector: 'app-tecnico-update',
+  templateUrl: './tecnico-update.component.html',
+  styleUrls: ['./tecnico-update.component.css']
+})
+export class TecnicoUpdateComponent implements OnInit {
+
+  tecnico : Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    telefone: ''
+  }
+
+  nome = new FormControl('', [Validators.minLength(5)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  telefone = new FormControl('', [Validators.minLength(10)])
+
+  constructor(private router : Router, private service : TecnicoService) { }
+
+  ngOnInit(): void {
+  }
+
+  
+
+  errorValidNome() {
+    if(this.nome.invalid){
+      return 'O nome deve ter entre 5 e 100 caracteres!';
+    }
+    return false;
+  }
+
+  errorValidCPF() {
+    if(this.cpf.invalid){
+      return 'O CPF deve ter entre 11 e 15 caracteres!';
+    }
+    return false;
+  }
+
+  errorValidTelefone() {
+    if(this.telefone.invalid){
+      return 'O telefone deve ter entre 10 e 18 caracteres!';
+    }
+    return false;
+  }
+
+  cancel(): void {
+    this.router.navigate(['tecnicos'])
+  }
+}
