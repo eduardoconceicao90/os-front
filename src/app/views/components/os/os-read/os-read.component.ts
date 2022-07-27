0a889e2ce389728dd2a0cbf84b7cafa0f613994c
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-27 12:01:11 -0300

    componente read para Ordens de Serviço Parte 1

---
+++ src/app/views/components/os/os-read/os-read.component.ts
@@ -0,0 +1,39 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { OS } from 'src/app/models/os';
+import { OsService } from 'src/app/services/os.service';
+
+@Component({
+  selector: 'app-os-read',
+  templateUrl: './os-read.component.html',
+  styleUrls: ['./os-read.component.css']
+})
+export class OsReadComponent implements AfterViewInit {
+
+  lista: OS[] = [];
+
+  displayedColumns: string[] = ['cliente', 'tecnico', 'abertura', 'fechamento', 'prioridade', 'status','acoes'];
+  dataSource = new MatTableDataSource<OS>(this.lista);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private service : OsService, private router: Router) {}
+
+  ngAfterViewInit() {  
+    this.findAll();
+  }
+  
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) =>{
+      this.lista = resposta;
+      this.dataSource = new MatTableDataSource<OS>(this.lista);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  navigateToCreate(): void {
+    this.router.navigate(['os/create'])
+  }
+}
