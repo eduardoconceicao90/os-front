4df6008416db72f238ab9711d2a07b36be30cfea
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-27 08:44:22 -0300

    componente update  para Cliente

---
+++ src/app/views/components/cliente/cliente-update/cliente-update.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+@Component({
+  selector: 'app-cliente-update',
+  templateUrl: './cliente-update.component.html',
+  styleUrls: ['./cliente-update.component.css']
+})
+export class ClienteUpdateComponent implements OnInit {
+
+  id_user = ''
+
+  cliente : Cliente = {
+    id: '',
+    nome: '',
+    cpf: '',
+    telefone: ''
+  }
+
+  nome = new FormControl('', [Validators.minLength(5)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  telefone = new FormControl('', [Validators.minLength(10)])
+
+  constructor(private router : Router, private service : ClienteService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.id_user = this.route.snapshot.paramMap.get('id')!
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.id_user).subscribe((resposta) => {
+      this.cliente = resposta;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.cliente).subscribe((resposta) =>{
+      this.router.navigate(['clientes'])
+      this.service.message('Cliente atualizado com sucesso!')
+    }, err => {
+      this.service.message('Validar se todos os campos estão preenchidos corretamente')
+    })
+  }
+
+  errorValidNome() {
+    if(this.nome.invalid){
+      return 'O nome deve ter entre 5 e 100 caracteres!';
+    }
+    return false;
+  }
+
+  errorValidCPF() {
+    if(this.cpf.invalid){
+      return 'O CPF deve ter entre 11 e 15 caracteres!';
+    }
+    return false;
+  }
+
+  errorValidTelefone() {
+    if(this.telefone.invalid){
+      return 'O telefone deve ter entre 10 e 18 caracteres!';
+    }
+    return false;
+  }
+
+  cancel(): void {
+    this.router.navigate(['clientes'])
+  }
+}
