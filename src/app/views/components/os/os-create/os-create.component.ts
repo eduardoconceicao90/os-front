ac67174040ff32f83aae3dc48c79a0d66581d0a1
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-27 16:21:34 -0300

    compoenete create para Ordem de Serviço parte 2

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
+import { OS } from 'src/app/models/os';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { OsService } from 'src/app/services/os.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-os-create',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OsCreateComponent implements OnInit {
 
-  constructor() { }
+  selected = ''
+
+  tecnicos: Tecnico[] = []
+
+  clientes: Cliente[] = []
+
+  constructor(private tecnicoService : TecnicoService, private clienteService : ClienteService) { }
 
   ngOnInit(): void {
+    this.listarTecnicos();
+    this.listarClientes();
+    
   }
 
+  listarTecnicos(): void {
+    this.tecnicoService.findAll().subscribe((resposta) => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  listarClientes(): void {
+    this.clienteService.findAll().subscribe((resposta) => {
+      this.clientes = resposta;
+    })
+  }
 }
