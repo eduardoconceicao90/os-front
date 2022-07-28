a6f86790f80e1578cbbf46cf9445e5bc712353a1
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-27 21:53:48 -0300

    compoenete update para Ordem de Serviço parte 1

--- src/app/views/components/os/os-update/os-update.component.ts
+++ src/app/views/components/os/os-update/os-update.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Cliente } from 'src/app/models/cliente';
 import { OS } from 'src/app/models/os';
 import { Tecnico } from 'src/app/models/tecnico';
@@ -8,11 +8,11 @@ import { OsService } from 'src/app/services/os.service';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
-  selector: 'app-os-create',
-  templateUrl: './os-create.component.html',
-  styleUrls: ['./os-create.component.css']
+  selector: 'app-os-update',
+  templateUrl: './os-update.component.html',
+  styleUrls: ['./os-update.component.css']
 })
-export class OsCreateComponent implements OnInit {
+export class OsUpdateComponent implements OnInit {
 
   selected = ''
 
@@ -28,17 +28,26 @@ export class OsCreateComponent implements OnInit {
 
   clientes: Cliente[] = []
 
-  constructor(private tecnicoService : TecnicoService, private clienteService : ClienteService, private service : OsService, private router : Router) { }
+  constructor(private tecnicoService : TecnicoService, private clienteService : ClienteService, private service : OsService, private router : Router, private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.os.id = this.route.snapshot.paramMap.get('id')
+    this.findById();
     this.listarTecnicos();
     this.listarClientes();
     
   }
 
-  create(): void {
+  findById(): void{
+    this.service.findById(this.os.id).subscribe((resposta) => {
+      this.os = resposta;
+      this.converteDados();
+    })
+  }
+
+  update(): void {
     this.service.create(this.os).subscribe((resposta) => {
-      this.service.message('Ordem de Serviço criada com sucesso!')
+      this.service.message('Ordem de Serviço atualizada com sucesso!')
       this.router.navigate(['os'])
     }, err => {
       this.service.message('Validar se todos os campos estão preenchidos corretamente')
@@ -60,4 +69,23 @@ export class OsCreateComponent implements OnInit {
   cancel(): void {
     this.router.navigate(['os'])
   }
+
+  converteDados(): void {
+    if(this.os.status == 'ABERTO'){
+      this.os.status = 0;
+    } else if (this.os.status == 'EM ANDAMENTO'){
+      this.os.status = 1;
+    } else {
+      this.os.status = 2;
+    }
+
+    if(this.os.prioridade == 'BAIXA'){
+      this.os.prioridade = 0;
+    } else if (this.os.prioridade == 'MÉDIA'){
+      this.os.prioridade = 1;
+    } else {
+      this.os.prioridade = 2;
+    }
+  }
 }
+
